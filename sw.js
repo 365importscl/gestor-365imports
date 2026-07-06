58726d3c72a4b854e1a9f768f5a597ea06c032b8
365importscl <logistica@365imports.cl>
2026-07-06 14:51:29 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = '365imports-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+ 
+// Instalar SW y cachear archivos básicos
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+ 
+// Activar SW y limpiar caches viejos
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
